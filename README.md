9e6721c1177848225fdb763a8179830a4fbc9386
ivanmagdic <magdic.ivan1@gmail.com>
2020-11-27 15:29:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Installation
+
+```bash
+composer install && npm i
+cp .env.example .env
+php artisan key:generate
+```
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    purge: [
+        './resources/views/**/*blade.php',
+    ],
+    theme: {
+    },
+    variants: {},
+    plugins: [],
+}
